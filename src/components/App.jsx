2ccd29037424fbe8431c956ba1cec12997aaa5f6
Danish-Ehsan <danish.ehsan321@gmail.com>
2023-07-12 17:08:00 -0400

    Removed unused "count" state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,6 @@ import Alarms from "components/Alarms";
 import "styles/App.scss";
 
 function App() {
-    const [count, setCount] = useState(0);
     const [date, setDate] = useState(new Date());
     const [timeRunning, setTimeRunning] = useState(true);
     const timeInterval = useRef(null);
