a476f7bd381f1f9250fa36afe8cbb61a0d56a393
Danish-Ehsan <danish.ehsan321@gmail.com>
2023-08-10 10:42:14 -0400

    Removed console.log

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -21,8 +21,6 @@ export default function Clock({
         date.getHours() * (360 / 12) +
         date.getMinutes() * (360 / (60 * 12));
 
-    console.log(date.getSeconds());
-
     useEffect(() => {
         //When clock needles are completing a full circle, the css rotation degrees need to advance instead of resetting to 0 so the needle doesn't go backwards a full rotation
         //This needs to happen after the render so it doesn't prematurely set the rotation ahead.
@@ -70,6 +68,7 @@ export default function Clock({
     return (
         <motion.div
             className={activeAlarms.length ? "clock alarm" : "clock"}
+            //if duration is 1 second when seconds reach back to 0 the colors rapidly cycle back to the start
             animate={{
                 "--bg-hue": timeHue,
                 transition: { duration: date.getSeconds() === 0 ? 0 : 1 }
